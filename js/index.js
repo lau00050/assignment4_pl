2e7701d63b91f1c6773fa3ed231c437aa34545af
lau00050 <phil.lauricella@gmail.com>
2018-08-12 18:15:43 +1000

    Version 2
    • Removed readme
    • Removed assignment reference image
    • Rearranged code to change from Read More to Read Less
    • Rearranged code to change from Learn More to Learn Less

--- js/index.js
+++ js/index.js
@@ -2,10 +2,40 @@ $('a').click(function() {
 	event.preventDefault()
 })
 
-$('.readmore').click(function() {
-	$('#readMoreText').slideToggle()
+// $('.readMore').click(function() {
+// 	$('#readMoreText').slideToggle()
+// })
+//
+// $('.learnmore').click(function() {
+// 	$('#learnMoreText').slideToggle()
+// })
+
+// ReadMore
+$('.readMore').click(function() {
+	$('#readMoreText').slideDown()
+	$('.readLess').slideDown()
+	$('.readMore').css('display', 'none')
 })
 
-$('.learnmore').click(function() {
-	$('#learnMoreText').slideToggle()
+$('.readLess').click(function() {
+	// console.log('test link')
+	$('#readMoreText').slideUp()
+	$('.readMore').slideUp()
+	$('.readLess').css('display', 'none')
+	$('.readMore').css('display', 'block')
+})
+
+// LearnMore
+$('.learnMore').click(function() {
+	$('#learnMoreText').slideDown()
+	$('.learnLess').slideDown()
+	$('.learnMore').css('display', 'none')
+})
+
+$('.learnLess').click(function() {
+	// console.log('test link')
+	$('#learnMoreText').slideUp()
+	$('.learnMore').slideUp()
+	$('.learnLess').css('display', 'none')
+	$('.learnMore').css('display', 'block')
 })
